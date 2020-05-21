d6dcd550972b0c627dc611b2946312b4c2c6265e
Vinny <viniciusgoula@gmail.com>
2020-05-21 11:51:05 -0600

    Initial Commit

---
+++ desktop/src/com/game/dc/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.game.dc.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.game.dc.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
