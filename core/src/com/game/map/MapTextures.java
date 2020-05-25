3de1b8766f5189120ec29b5468ff4c0bbd0c0095
lpelajo <65730548+lpelajo@users.noreply.github.com>
2020-05-25 18:04:55 -0300

    .

--- core/src/com/game/map/MapTextures.java
+++ core/src/com/game/map/MapTextures.java
@@ -5,29 +5,20 @@
  */
 package com.game.map;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.game.entities.characters.CharacterEntity;
 /**
  *
  * @author lpela
  */
-public class MapTextures extends ApplicationAdapter {
-    static private SpriteBatch batch;
-    static private Texture floor, wall, wallTop, plaster;
+public class MapTextures {
     private int textureType;
-/*
-    switch (textureType) {
-        case 1:
-            @Override
-            public void create() {
-                batch = new SpriteBatch();
-                floor = new Texture("floor33.gif");
-                wall = new Texture("wall12.gif");
-                wallTop = new Texture("roomWall33.gif");
-                plaster = new Texture("roomFloor22.gif");
-            }
-            break;
-    }*/
+
+    CharacterEntity floor = new CharacterEntity(new Texture("floor33.gif"));
+    CharacterEntity wall = new CharacterEntity(new Texture("wall12.gif"));
+    CharacterEntity wallTop = new CharacterEntity(new Texture("roomWall33.gif"));
+    CharacterEntity plaster = new CharacterEntity(new Texture("roomFloor22.gif"));
     
+    com.game.dc.MainGame.mapLayer.getObjects().add(floor);
 }
