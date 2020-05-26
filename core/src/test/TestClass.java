bc242868c6a4f0c665a42bd4d01e20345ddbcbf9
Vinny <viniciusgoula@gmail.com>
2020-05-26 13:47:53 -0600

    1

---
+++ core/src/test/TestClass.java
@@ -0,0 +1,28 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.game.map.DungeonMap;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestClass {
+
+    DungeonMap dungeonMap;
+
+    @BeforeEach
+    public void initializer() {
+        dungeonMap = new DungeonMap();
+    }
+
+    @Test
+    public void layerAddLayerWhenThereAreNoLayers(){
+        dungeonMap.
+    }
+    
+    @Test
+    public void layerAddLayerAtIndex1() {
+
+    }
+}
